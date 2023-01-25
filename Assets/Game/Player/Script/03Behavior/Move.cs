8f00502b788176ea76f5422702cc3b5de25148dc
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-01-25 17:38:40 +0900

    [add] 移動,ジャンプ振る舞いを追加

---
+++ Assets/Game/Player/Script/03Behavior/Move.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    [System.Serializable]
+    public class Move
+    {
+        [SerializeField]
+        private float _moveSpeed = 1f;
+
+        private float _horizontalInput = default;
+        private Rigidbody2D _rigidbody2D = null;
+
+        public void Init(Rigidbody2D rigidbody2D)
+        {
+            _rigidbody2D = rigidbody2D;
+        }
+
+        public void OnMovementControl(InputAction.CallbackContext callbackContext)
+        {
+            // ì¸óÕÇéÊìæÇ∑ÇÈ
+            _horizontalInput = callbackContext.ReadValue<Vector2>().x;
+        }
+        public void EndMovementControl(InputAction.CallbackContext _)
+        {
+            // ë¨ìxÇè¡Ç∑
+            _horizontalInput = 0f;
+        }
+        public void Update()
+        {
+            // Rigidbody2DÇ…ë¨ìxÇìnÇ∑
+            _rigidbody2D.velocity = new Vector2(
+                _horizontalInput * _moveSpeed,
+                _rigidbody2D.velocity.y
+                );
+        }
+    }
+}
